87124e1d5174af1516e1c78dfae925d0f19de4c0
ligeronahill <provotorov@inbox.ru>
2023-10-31 11:18:44 +0300

    chapter 2

--- cmd/main.go
+++ cmd/main.go
@@ -7,26 +7,26 @@ import (
 )
 
 func main() {
-  addr := ":8080"
-  mux := http.NewServeMux()
-  mux.HandleFunc("/hello", func(w http.ResponseWriter,_ *http.Request) {
-    enc:= json.NewEncoder(w)
-    w.Header().Set("Content-Type", "application/json; charset=utf-8")
-    resp := Resp{
-      Language: "English",
-      Translation: "Hello",
-    }
-    if err := enc.Encode(resp); err != nil {
-      panic("unable to encode response")
-    }
-  })
+	addr := ":8080"
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", func(w http.ResponseWriter, _ *http.Request) {
+		enc := json.NewEncoder(w)
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		resp := Resp{
+			Language:    "English",
+			Translation: "Hello",
+		}
+		if err := enc.Encode(resp); err != nil {
+			panic("unable to encode response")
+		}
+	})
 
-  log.Printf("Listening on %s\n", addr)
+	log.Printf("Listening on %s\n", addr)
 
-  log.Fatal(http.ListenAndServe(addr, mux))
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
 
 type Resp struct {
-  Language string `json:"language"`
-  Translation string `json:"translation"`
+	Language    string `json:"language"`
+	Translation string `json:"translation"`
 }
